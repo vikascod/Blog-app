43399b66e020495a832ce59fe0c26d0d79fe1aed
Vikas <vikas1618072@gmail.com>
2023-04-06 11:12:25 +0530

    added static root

--- Blogpost/settings.py
+++ Blogpost/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 from django.utils.crypto import get_random_string
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,10 +154,13 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 #set default user pic
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-STATIC_ROOT = (BASE_DIR / 'static')
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+   ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
