ec9e7ac09f81abe4089393df610804bcbe3438ae
Vikas <vikas1618072@gmail.com>
2023-04-06 11:27:41 +0530

    change in settings

--- Blogpost/settings.py
+++ Blogpost/settings.py
@@ -158,7 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES = (BASE_DIR / 'static',)
+# STATICFILES = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
