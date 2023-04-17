5a6bc3f35377927f6d7afaa250508a9a26d06bc5
Vikas <vikas1618072@gmail.com>
2023-04-17 17:57:02 +0530

    added default secret key

--- Blogpost/settings.py
+++ Blogpost/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', "HIDGHSIoydogodghodhooh98w94tcBIUt&65$%^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
